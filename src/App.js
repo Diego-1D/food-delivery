991fe656e7c394c2d087c443a0a17b2aefbb839b
Diego-1D <diego1d_fernandes@outlook.com>
2022-05-23 23:44:38 -0300

    Configurações das rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Header from "./components/Header";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import {Header, MainContainer, CreateContainer} from "./components";
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <AnimatePresence exitBeforeEnter>
+      <div className='w-screen h-auto flex flex-col bg-primary'>
+        <Header />
+        <main className='mt-24 p-8 w-full'>
+          <Routes>
+            <Route path='/*' element={<MainContainer />} />
+            <Route path='/createItem' element={<CreateContainer />} />
+          </Routes>
+        </main>
+      </div>
+    </AnimatePresence>
   );
 }
 
