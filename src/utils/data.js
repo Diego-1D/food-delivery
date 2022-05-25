e9f3f5b4cbd79a8bb8bc493514c879c46e5f2dcf
Diego-1D <diego1d_fernandes@outlook.com>
2022-05-24 23:30:25 -0300

    Criação da base de dados do componente Header

---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+import I1 from "../img/i1.png"
+import F1 from "../img/f1.png"
+
+export const heroData = [
+    {
+        id: 1,
+        name: "Icecream",
+        decp: "Chocolate & vanilla",
+        price: '5.25',
+        imageSrc: I1
+    },
+    {
+        id: 2,
+        name: "Strawberries",
+        name: "Strawberries",
+        decp: "Fresh ",
+        price: '5.25',
+        imageSrc: F1
+    },
+    {
+        id: 1,
+        name: "Icecream",
+        decp: "Chocolate & vanilla",
+        price: '5.25',
+        imageSrc: I1
+    },
+    {
+        id: 2,
+        name: "Strawberries",
+        name: "Strawberries",
+        decp: "Fresh ",
+        price: '5.25',
+        imageSrc: F1
+    }
+]
