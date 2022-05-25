0a7f27caae7de638b62bc5595739c6bd60e098bf
Diego-1D <diego1d_fernandes@outlook.com>
2022-05-24 23:29:12 -0300

    Criação do componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,99 +1,121 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useState } from 'react'
+
+import { MdShoppingBasket, MdLogout, MdAdd } from 'react-icons/md'
+import { motion } from 'framer-motion'
+
+import { useStateValue } from '../context/StateProvider'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { actionType } from '../context/reducer'
+import { app } from '../firebase.config'
+import { Link } from "react-router-dom"
+
 import logo from '../img/logo.png'
 import avatar from '../img/avatar.png'
-import { MdShoppingCart, MdLogout, MdAdd } from 'react-icons/md'
-import { useNavigate, NavLink } from "react-router-dom"
-import { AnimatePresence, motion } from 'framer-motion'
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-import { isMobileCordova } from '@firebase/util'
-import { useStateValue } from '../context/StateProvider'
-import { app } from '../firebase.config'
-import { actionType } from '../context/reducer'
 
 
 const Header = () => {
     const firebaseAuth = getAuth(app)
     const provider = new GoogleAuthProvider()
+
     const [{ user }, dispatch] = useStateValue()
 
-    const [cartMenu, setCartMenu] = useState(false)
     const [isMenu, setIsMenu] = useState(false)
-    const [isMobileMenu, setIsMobileMenu] = useState(false)
-
-
 
     const login = async () => {
         if (!user) {
-            const { user } = await signInWithPopup(firebaseAuth, provider)
-            const { refreshToken, providerData } = user
-            localStorage.setItem("user", JSON.stringify(providerData[0]))
+            const {
+                user: { refreshToken, providerData }
+            } = await signInWithPopup(firebaseAuth, provider)
             dispatch({
                 type: actionType.SET_USER,
                 user: providerData[0],
             })
+            localStorage.setItem("user", JSON.stringify(providerData[0]))
         } else {
             setIsMenu(!isMenu)
-            isMobileCordova(false)
         }
+    }
+
+    const logout = () => {
+        setIsMenu(false)
+        localStorage.clear()
+
+        dispatch({
+            type: actionType.SET_USER,
+            user: null
+        })
     }
 
 
     return (
-        <div className='w-full px-2 lg:px-8 py-2 flex items-center justify-cebter'>
-            {/*Desktop Menu*/}
-            <div className='w-full hidden md:flex items-center'>
-                {/*User Menu*/}
-                <div className='mr auto relative'>
-                    <motion.img
-                        whileTap={{ scale: 0.8 }}
-                        src={user ? user?.photoURL : avatar}
-                        className='w-14 h-14 cursor-pointer rounded-full shadow-xl'
-                        alt=""
-                        onClick={login}
-                    />
+        <div className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary'>
+            {/*Desktop & Tablet*/}
+            <div className='hidden md:flex w-full h-full items-center justify-between'>
+                <Link to={"/"} className="flex items-center gap-2">
+                    <img src={logo} className="w-8 object-cover" alt="logo" />
+                    <p className='text-headingColor text-x1 font-bold'>City</p>
+                </Link>
+
+                <div className='flex items-center gap-8'>
+                    <motion.ul
+                        initial={{ opacity: 0, x: 200 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: 200 }}
+                        className="flex items-center gap-24"
+                    >
+                        <li className="text-lg text-textColor hover: text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                            Home
+                        </li>
+                        <li className="text-lg text-textColor hover: text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                            Menu
+                        </li>
+                        <li className="text-lg text-textColor hover: text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                            About Us
+                        </li>
+                        <li className="text-lg text-textColor hover: text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                            Service
+                        </li>
+                    </motion.ul>
+                    <div className='relative flex items-center justify-center'>
+                        <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' />
+                        <p>My Cart</p>
+                    </div>
+
+                    <div className='relative'>
+                        <motion.img
+                            whileTap={{ scale: 0.6 }}
+                            src={user ? user.photoURL : avatar}
+                            className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full'
+                            alt="userprofile"
+                            onClick={login}
+                        />
+                    </div>
                     {isMenu && (
                         <motion.div
                             initial={{ opacity: 0, scale: 0.6 }}
                             animate={{ opacity: 1, scale: 1 }}
                             exit={{ opacity: 0, scale: 0.6 }}
-                            className="w-40 bg-cardColor rounded-lg shadow-2xl absolute top-16 -left-2 z-30">
+                            className="w-40 bg-gray-50 rounded-lg shadow-xl flex flex-col absolute top-12 right-0">
                             {
-                                user.email === "diego1d_fernandes@outlook.com" && (
-                                    <NavLink to={"/createItem"} onClick={() => setIsMenu(false)}>
-                                        <p className='px-4 py-2 cursor-pointer hover:bg-slate-200 flex items-center gap-3'>
+                                user && user.email === "diego1d_fernandes@outlook.com" && (
+                                    <Link to={"/createItem"}>
+                                        <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+                                            onClick={() => setIsMenu(false)}>
                                             New Item <MdAdd />
                                         </p>
-                                    </NavLink>
+                                    </Link>
                                 )}
-                            <p className='px-4 py-2 cursor-pointer hover:bg-slate-200 flex items-center gap-3'>
+
+                            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+                                onClick={logout}>
                                 Lougout <MdLogout />
                             </p>
                         </motion.div>
                     )}
                 </div>
-                {/*Menu Actions*/}
-                <div className='flex items-center'>
-                    <NavLink to='/'>
-                        Menu
-                    </NavLink>
-                    <NavLink to='/'>
-                        <img src={logo} className="w-40 mx-16" alt="" />
-                    </NavLink>
-                    <NavLink to='/'>
-                        Contact
-                    </NavLink>
-                </div>
-                <motion.div
-                    whileTap={{ scale: 0.8 }}
-                    className="flex items-center gap-2 bg-black px-5 py-4 rounded-md ml-auto cursor-pointer relative"
-                    onClick={() => setCartMenu(!cartMenu)}
-                >
-                    <MdShoppingCart className='text-xl text-white' />
-                    <p className='text-base text-white font-semibold'>My Cart</p>
-                </motion.div>
             </div>
             <div>
-                
+                {/* Mobile */}
             </div>
         </div>
     )
