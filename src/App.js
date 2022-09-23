8f5659a233c0cea4d0e5fd379203665fa4abebc0
Diego-1D <diego1d_fernandes@outlook.com>
2022-09-22 21:34:11 -0300

    basename

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header, MainContainer, CreateContainer } from "./components";
 import { AnimatePresence } from 'framer-motion';
 import { actionType } from './context/reducer';
 import { useStateValue } from './context/StateProvider';
-import {getAllFoodItems} from './utils/firebaseFunction'
+import { getAllFoodItems } from './utils/firebaseFunction'
 
 
 function App() {
@@ -24,17 +24,19 @@ function App() {
   }, [])
 
   return (
-    <AnimatePresence exitBeforeEnter>
-      <div className='w-screen h-auto flex flex-col bg-primary'>
-        <Header />
-        <main className='mt-14 md:mt-20 px-4 md:px-16 py-4 w-full'>
-          <Routes>
-            <Route exact path='/' element={<MainContainer />} />
-            <Route path='/createItem' element={<CreateContainer />} />
-          </Routes>
-        </main>
-      </div>
-    </AnimatePresence>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <AnimatePresence exitBeforeEnter>
+        <div className='w-screen h-auto flex flex-col bg-primary'>
+          <Header />
+          <main className='mt-14 md:mt-20 px-4 md:px-16 py-4 w-full'>
+            <Routes>
+              <Route exact path='/' element={<MainContainer />} />
+              <Route path='/createItem' element={<CreateContainer />} />
+            </Routes>
+          </main>
+        </div>
+      </AnimatePresence>
+    </BrowserRouter>
   );
 }
 
